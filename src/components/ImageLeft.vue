ca8da1c0da2af3cb30e989c5b4a85cbf143b216a
NadjaJust <felis.monstermietz@lf-net.org>
2026-02-09 09:54:59 +0100

    Started Website Projekt for Research Project

---
+++ src/components/ImageLeft.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  type Props = {
+    language : 'de' | 'eng',
+    head: string,
+    content: text,
+    imgSrc: string
+  }
+</script>
+
+<template>
+  <div class="content">
+    <div class="image">
+
+    </div>
+    <div class="text">
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
