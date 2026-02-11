8060e4388bf3054c48e8b543c65bf6b0887e8aa8
NadjaJust <felis.monstermietz@lf-net.org>
2026-02-11 18:43:24 +0100

    Little changes at nav

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -4,15 +4,19 @@
 
 <template>
   <nav class="sidenav">
-    <a href ="#section1">Herzlich Willkommen</a>
-    <a href ="#section2">Inhalt und Ablauf</a>
-    <a href ="#section3">Datenschutz</a>
-    <a href ="#section4">Verwendung der Daten</a>
-    <a href ="#section5">Hilfsmittel</a>
-    <a href ="#section6">Kontakt</a>
+    <a href ="#section1">Willkommen</a><br>
+    <a href ="#section2">Inhalt und Ablauf</a><br>
+    <a href ="#section3">Datenschutz</a><br>
+    <a href ="#section4">Verwendung der Daten</a><br>
+    <a href ="#section5">Hilfsmittel</a><br>
+    <a href ="#section6">Kontakt</a><br>
   </nav>
 </template>
 
 <style scoped>
-
+.sidenav {
+  font-family: 'Bebas Neue', sans-serif;
+  font-weight: 700;
+  font-size: 2em;
+}
 </style>
